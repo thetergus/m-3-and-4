09a261a5adcc351c26bcc4d44018c7fae4e79e81
Tergus <tergus@gmail.com>
2023-05-23 17:48:29 +1200

    Before merging after changing server with Heni help

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import env from "dotenv";
 import carRoutes from "./routes/carRoutes";
+import carInsuranceQuote from "./routes/carInsuranceRoutes";
+
 
 env.config();
 
@@ -10,6 +12,8 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/", carRoutes);
+server.use("/", carInsuranceQuote);
+
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => {
