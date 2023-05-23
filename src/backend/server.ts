27c9f75c2b69303900d3e64db53d214d90923352
Heni Kimura <henace86@gmail.com>
2023-05-23 23:15:16 +1200

    edit server

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,16 +1,10 @@
-import express from "express";
-import cors from "cors";
 import env from "dotenv";
 import app from "./app";
 
 env.config();
 
-const server = express();
-server.use(cors());
-server.use(express.json());
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
-
-export { server, app };
